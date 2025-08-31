b2e9663d0d0d182ee7da6dacb7335b2c70e820e5
PAPAJI0007 <kapilpundit420@gmail.com>
2025-08-31 23:53:04 +0530

    Delete modules/commands directory

--- modules/commands/tid.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'tid',
-  async execute({ api, event, broadcastLog }) {
-    api.sendMessageMqtt(`Group ID: ${event.threadID}`, event.threadID, event.messageID);
-    broadcastLog(`Displayed thread ID: ${event.threadID}`);
-  }
-};
